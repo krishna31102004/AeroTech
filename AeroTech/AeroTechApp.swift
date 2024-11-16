460f996319c73cee5c0acb36afc30332873bb014
Krishna Balaji <krishna311004@gmail.com>
2024-11-16 12:48:22 -0700

    Initial Commit

---
+++ AeroTech/AeroTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroTechApp.swift
+//  AeroTech
+//
+//  Created by Krishna Balaji on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct AeroTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AeroTechTests/AeroTechTests.swift
@@ -0,0 +1,17 @@
+//
+//  AeroTechTests.swift
+//  AeroTechTests
+//
+//  Created by Krishna Balaji on 11/16/24.
+//
+
+import Testing
+@testable import AeroTech
+
+struct AeroTechTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
